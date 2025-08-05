14768e5b34ba343a68acd03b0849a9d566251c82
Orewa-veer <pratiharvirendra@gmail.com>
2025-08-05 13:10:38 +0530

    test: populating db

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 import request from "supertest";
 import app from "../../app.js";
 import mongoose from "mongoose";
+import { Genre } from "../../models/genre.js";
 beforeAll(async () => {
   await mongoose.connect("mongodb://localhost/vidly_test", {
     useNewUrlParser: true,
@@ -11,13 +12,21 @@ beforeAll(async () => {
 afterAll(async () => {
   await mongoose.disconnect();
 });
-
-let server;
+afterEach(async () => {
+  await Genre.deleteMany({});
+});
 describe("/api/genres", () => {
   describe("Get/", () => {
     it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
+      //
       const res = await request(app).get("/api/genres");
       expect(res.status).toBe(200);
-    }, 10000);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === "genre1"));
+    });
   });
 });
